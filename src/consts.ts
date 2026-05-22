3fd84112990f8416dd3f2fbd7a5bcaba86d01d7b
Ben Cirrus <31079074+bencirrus@users.noreply.github.com>
2026-05-22 12:06:35 -0700

    Update site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "OctaBit.work";
+export const SITE_DESCRIPTION = "Adapt to AI | OctaBit.work";
