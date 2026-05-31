94335f522cf29bcf80dbe1ad11ff04001d96c58c
BC <ben.cirrus@me.com>
2026-05-30 18:51:46 -0700

    Configure for Cloudflare Workers deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-        site: "https://example.com",
+        site: "https://octabit.work",
         integrations: [mdx(), sitemap()],
+        adapter: cloudflare(),
         server: {
                 host: "0.0.0.0",
                 port: 5000,
