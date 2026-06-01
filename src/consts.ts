d5db92ff3622fd06c9007736bdbf8f8acc020e69
BC <ben.cirrus@me.com>
2026-05-31 21:00:38 -0700

    Save repo changes including deletions

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "OctaBit.work";
-export const SITE_DESCRIPTION = "Adapt to AI | OctaBit.work";
+export const SITE_DESCRIPTION = "Augment w/ Intelligence | OctaBit.work";
